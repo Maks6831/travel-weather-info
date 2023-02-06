462e05e4a108ff2ac3f22f4529128a758a9ce276
Makadelius <muhammadkhan@Muhammads-MacBook-Air.local>
2023-02-06 17:32:18 +0000

    current weather forecast in full working order.

--- script.js
+++ script.js
@@ -2,13 +2,26 @@ let apiKey = '3e1093f5d85b4cf539a5a5256729c7de';
 let apiKeyno2 = '29ce0cb407b2ddf0f16918761f1b5b94'
 
 
-function weatherInfo(latitude, longitude){
-    let forecastUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat='+ latitude +'&lon='+ longitude +'&appid=' + apiKeyno2;
+function currentWeatherInfo(latitude, longitude, input){
+    let forecastUrl = 'http://api.openweathermap.org/data/2.5/weather?lat='+ latitude +'&lon='+ longitude +'&units=metric&appid=' + apiKeyno2;
     $.ajax({
         url: forecastUrl,
         method: "GET",
     }). then(function(response){
         console.log(response);
+        let date = moment.unix(response.dt).format("DD/MM/YYYY");
+        let iconData = response.weather[0].icon;
+        console.log(iconData);
+        let source = 'http://openweathermap.org/img/wn/'+ iconData + '@2x.png';
+        let icon = $("<img>").attr('src', source);
+        let location = $('<h1>').text(input + ' ' + date).append(icon);
+        let description = $("<p>").text(response.weather[0].description + '!');
+        let temp = $("<p>").text('Temp: ' + response.main.temp + ' °C');
+        let wind = $("<p>").text('Wind: ' + response.wind.speed + ' KPH');
+        let humidity = $("<p>").text('Humidity: ' + response.main.humidity + ' °C');
+        
+        $("#today").append(location, description, temp, wind, humidity);
+    
     })
 
 
@@ -29,7 +42,7 @@ $("#search-form").submit(function(event){
         let latitude = response[0].lat;
         console.log(response[0].lon);
         let longitude = response[0].lon;
-        weatherInfo(latitude, longitude);
+        currentWeatherInfo(latitude, longitude, input);
       })
 
     })
