bd248ea0daad4cc13710bff16c671633484e8fda
Makadelius <muhammadkhan@Muhammads-MacBook-Air.local>
2023-02-07 17:27:02 +0000

    Added style to cards, filled out readme and commented script.js

--- script.js
+++ script.js
@@ -1,39 +1,43 @@
 $(document).ready(function(){
-
-
-
+//--------------------------------------------------------- global variables -----------------------------------------------//
+// note: Somehow using multiple keys minimized the number of errors when requesting information from API
 let apiKey = '3e1093f5d85b4cf539a5a5256729c7de';
 let apiKeyno2 = '29ce0cb407b2ddf0f16918761f1b5b94';
 let apiKeyno3 = 'f99a2eb7e96df9308bdccc7005ec552e';
-let searchHistory = [];
+let searchHistory = []; // array which stories city data
 
+//--------------------------------------------------------- storing search values to localstorage -----------------------------------------------//
+
+//stores the value of the search as well as being used to call the function which creates the button. 
 function addtohistory(latitude, longitude, input){
+    // store lat, long and name as an object.
     let cityData = {
         input: input,
         latitude: latitude,
         longitude: longitude,
     }
+    // check to see if city has already been stored (to prevent identical buttons being created)
     let index = searchHistory.findIndex(x => x.input === input);
     console.log(typeof index);
     if(index != -1){
         return
     } else {
-        console.log('input = ', input);
-        console.log('index = ', index);
-        
+        // push data to localstorage as an object inside of an array
         searchHistory.push(cityData);
         localStorage.setItem('search-history', JSON.stringify(searchHistory))
+        // create Buttons!!!
         createButton(latitude, longitude, input);
     }
     
 }
 
+//--------------------------------------------------------- display previous searches on reload -----------------------------------------------//
+// upon site load, buttons of previous searches are stored on the page. 
 function displaySearchHistory(){
     let storedCities = localStorage.getItem('search-history');
     if(storedCities){
         searchHistory = JSON.parse(storedCities);
     }
-    //for(let i = searchHistory.length -1; i >= 0; i--){
     for(let i = 0; i < searchHistory.length; i++){
         createButton(searchHistory[i].latitude, searchHistory[i].longitude, searchHistory[i].input);
     }
@@ -42,7 +46,7 @@ displaySearchHistory();
 
 
 
-
+//creat button which when pressed calls the API functions. 
 function createButton(latitude, longitude, input){
     let button = $('<button type="button" class="btn btn-primary">').text(input);
     button.addClass('buttonstyle');
@@ -54,6 +58,9 @@ function createButton(latitude, longitude, input){
     })
 }
 
+//--------------------------------------------------------- five day forecast! ---------------------------------------------------------------//
+
+// function which takes in the lat and long values from the geocoding api search and calculates five days forecast. 
 function fiveDayForecast(latitude, longitude){
     let fiveDayUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat='+ latitude + '&lon='+ longitude +'&units=metric&appid=' + apiKey;
     $.ajax({
@@ -63,13 +70,14 @@ function fiveDayForecast(latitude, longitude){
         $('#forecast').empty();
         console.log(response);
         let object = response.list
+        // for loop which runs through the length of the list in API object.
         for(let i = 0; i < object.length; i++){
             let timeData = response.list[i].dt_txt
+            //creates cards for each list which describes the weather at 12pm. 
             if(timeData.includes("12:00:00")){
-                console.log(response.list[i]);
                 let objectI = response.list[i];
                 let card = $('<div class="card" style="width: 18rem;">');
-                let cardBody = $('<div class="card-body">')
+                let cardBody = $('<div class="card-body card-shadow">')
                 let iconData = response.list[i].weather[0].icon;
                 let source = 'http://openweathermap.org/img/wn/'+ iconData + '@2x.png';
                 let icon = $("<img>").attr('src', source);
@@ -77,6 +85,7 @@ function fiveDayForecast(latitude, longitude){
                 let temp = $("<p>").text('Temp: ' + objectI.main.temp + ' °C');
                 let wind = $("<p>").text('Wind: ' + objectI.wind.speed + ' KPH');
                 let humidity = $("<p>").text('Humidity: ' + objectI.main.humidity + ' °C');
+                // uses the unix time stamp to display date
                 let date = $("<p>").text(moment.unix(objectI.dt).format("DD/MM/YYYY"));
                 cardBody.append(date, icon, temp, wind, humidity);
                 card.append(cardBody);
@@ -87,7 +96,9 @@ function fiveDayForecast(latitude, longitude){
     })
 }
 
+//--------------------------------------------------------- Current weather -----------------------------------------------------------------------//
 
+// function which takes in the lat and long values from the geocoding api search and calculates current weather using current weather api. 
 function currentWeatherInfo(latitude, longitude, input){
     let forecastUrl = 'http://api.openweathermap.org/data/2.5/weather?lat='+ latitude +'&lon='+ longitude +'&units=metric&appid=' + apiKeyno2;
     $.ajax({
@@ -96,6 +107,7 @@ function currentWeatherInfo(latitude, longitude, input){
     }). then(function(response){
         $("#today").empty();
         console.log(response);
+        // uses the unix time stamp to display date
         let date = moment.unix(response.dt).format("DD/MM/YYYY");
         let iconData = response.weather[0].icon;
         console.log(iconData);
@@ -114,7 +126,8 @@ function currentWeatherInfo(latitude, longitude, input){
     })
 }
 
-
+//--------------------------------------------------------- Event Listener -----------------------------------------------------------------------//
+// Event listener for search querry - calculates the lat and long values. 
 $("#search-form").submit(function(event){
     event.preventDefault();
     let input = $("#search-input").val().trim();
@@ -128,9 +141,13 @@ $("#search-form").submit(function(event){
         let latitude = response[0].lat;
         console.log(response[0].lon);
         let longitude = response[0].lon;
+        // call current weather
         currentWeatherInfo(latitude, longitude, input);
+        //call five day forecast
         fiveDayForecast(latitude, longitude);
+        // stores city data as well as creating the button. 
         addtohistory(latitude, longitude, input);
+       
       })
 
     })
@@ -139,10 +156,6 @@ $("#search-form").submit(function(event){
 
 
 
-
-
-
-// query url for 16 day forecast
 
 
 })
